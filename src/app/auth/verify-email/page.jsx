b3d20820360d5c8cba631eb43adfce9fc59646e8
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-06-11 16:34:11 +0600

    suspense add

--- src/app/auth/verify-email/page.jsx
+++ src/app/auth/verify-email/page.jsx
@@ -1,11 +1,23 @@
+
+import { Suspense } from 'react';
 import VerifyEmailBox from "@/components/auth/VerifyEmailBox";
 
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen text-[#333333]">
+      Loading email verification form...
+    </div>
+  );
+};
+
 const VerifyEmailPage = () => {
-    return (
-        <>
-            <VerifyEmailBox/>
-        </>
-    );
+  return (
+    <>
+      <Suspense fallback={<Loading />}>
+        <VerifyEmailBox/>
+      </Suspense>
+    </>
+  );
 };
 
 export default VerifyEmailPage;
